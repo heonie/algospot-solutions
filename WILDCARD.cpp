1928bb915420bf4e7c495ef97a3be3aa4f4fc99e
Kookheon Kim <heonie@gmail.com>
2017-12-14 13:13:58 +0900

    Accepted

--- WILDCARD.cpp
+++ WILDCARD.cpp
@@ -1,36 +1,68 @@
 #include <iostream>
-#include <cstdlib>
 #include <cstring>
 using namespace std;
 
 int N;
 char wildcard[101];
 char names[50][101];
+int cache[101][101];
 
 int compare(const void *a, const void *b) {
 	return strcmp((const char *)a, (const char *)b);
 }
 
-bool match(char *w, char *name) {
-	if (*w == '\0' && *name == '\0') {
+bool match(const char *wc, const char *s) {
+	while (*wc && *s && (*wc == '?' || *wc == *s)) {
+		wc++;
+		s++;
+	}
+	if (!*wc) {	// end of wildcard
+		return *s == '\0';
+	}
+	else if (!*s) {	// end of string, but wildcard remained
+		while (*wc) {
+			if (*wc != '*') {
+				return false;
+			}
+			wc++;
+		}
 		return true;
 	}
-	else if (*w == '\0') {
+	else if (*wc == '*') {
+		return match(wc + 1, s) || match(wc, s + 1);
+	}
+	else {
 		return false;
 	}
+}
 
-	if (*w == '?' || *w == *name) {
-		return match(w + 1, name + 1);
+int matchDP(const char *wc, int w, const char *name, int s) {
+	int &rst = cache[w][s];
+	if (rst != -1) {
+		return rst;
 	}
-	else if (*w == '*') {
-		int len = strlen(name);
-		for(int i=0; i<=len; i++) {
-			if (match(w + 1, name + i)) {
-				return true;
+	while (wc[w] && name[s] && (wc[w] == '?' || wc[w] == name[s])) {
+		w++;
+		s++;
+	}
+	if (!wc[w]) {	// end of wildcard
+		return rst = (name[s] == '\0') ? 1 : 0;
+	}
+	else if (!name[s]) {	// end of string, but wildcard remained
+		while (wc[w]) {
+			if (wc[w] != '*') {
+				return (rst = 0);
 			}
+			w++;
 		}
+		return (rst = 1);
 	}
-	return false;
+	else if (wc[w] == '*') {
+		return (rst = (matchDP(wc, w + 1, name, s) || matchDP(wc, w, name, s + 1)) ? 1 : 0);
+	}
+	else {
+		return (rst = 0);
+	}
 }
 
 int main() {
@@ -44,7 +76,9 @@ int main() {
 		}
 		qsort(names, N, sizeof(char) * 101, compare);
 		for (int i = 0; i < N; i++) {
-			if (match(&wildcard[0], &names[i][0])) {
+			//if (match(&wildcard[0], &names[i][0])) {
+			memset(cache, -1, sizeof(int) * 101 * 101);
+			if (matchDP(wildcard, 0, names[i], 0)) {
 				cout << names[i] << endl;
 			}
 		}
